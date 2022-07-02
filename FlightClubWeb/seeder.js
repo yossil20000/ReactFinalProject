f317039ace32990a14d694fb38a29267d2aa3bb5
YLPilot <yos.1965@gmail.com>
2022-07-02 17:14:27 +0300

    Web,
    Add Seeder

---
+++ FlightClubWeb/seeder.js
@@ -0,0 +1,64 @@
+var mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const connectDB = require('./database/db')
+var Member = require('./Models/member');
+var device = require('./Models/device');
+const FlightReservation = require('./Models/flightReservation');
+const { rejectSeries } = require('async');
+const {DateTime} = require('luxon');
+connectDB()
+let members= null;
+let devices = null;
+let reservations = null;
+const createReservation = (member,device,date_from,date_to) =>{
+    let reservation = new FlightReservation();
+    reservation.member = member._id;
+    reservation.device = device._id;
+    reservation.date_from = date_from;
+    reservation.date_to = date_to;
+    reservation.notification.type = "email";
+    reservation.notification.notify = true;
+    console.log("date_from", date_from);
+    return reservation;
+}
+
+const importData = async () => {
+  try {
+    members = await Member.find();
+    devices = await device.find();
+    //console.log('Member Data Imported!' , members);
+    //console.log('Devices Data Imported!' , devices);
+    
+    if(members.length > 0 && devices.length > 0)    {
+      let newReservation = createReservation(members[0], devices[0], new Date(1995, 11, 17, 3, 4, 0), new Date(1995, 11, 17, 3, 5, 0));
+      await FlightReservation.insertMany(newReservation)
+      devices[0].flight_reservs = newReservation;
+		  members[0].flight_reservs = newReservation;
+      await devices[0].save();
+      await members[0].save();
+    }
+    process.exit();
+    
+  } catch (error) {
+    console.error(`${error}`)
+    process.exit(1)
+  }
+}
+
+
+const destroyData = async () => {
+  try {
+ 
+    console.log('Data Destroyed!')
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`)
+    process.exit(1)
+  }
+}
+
+if (process.argv[2] === '-d') {
+  destroyData()
+} else {
+  importData()
+}
