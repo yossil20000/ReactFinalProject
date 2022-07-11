2f685b654fde4d392da6c8886004053e92e11ab6
YLPilot <yos.1965@gmail.com>
2022-07-11 17:25:38 +0300

    client

--- FlightClubClientquery/src/pages/Home/components/welocome.tsx
+++ FlightClubClientquery/src/pages/Home/components/welocome.tsx
@@ -13,26 +13,6 @@ import { useQuery } from 'react-query';
 import ClubMessagesService from '../../../api/services/ClubMessages/ClubMessagesService';
 import { textAlign } from '@mui/system';
 
-let stepses = [
-  {
-    title: 'Select campaign settings',
-    description: `For each ad campaign that you create, you can control how much
-              you're willing to spend on clicks and conversions, which networks
-              and geographical locations you want your ads to show on, and more.`,
-  },
-  {
-    title: 'Create an ad group',
-    description:
-      'An ad group contains one or more ads which target a shared set of keywords.',
-  },
-  {
-    title: 'Create an ad',
-    description: `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`,
-  },
-];
 
 export default function TextMobileStepper() {
     const [steps,setSteps] = React.useState<ClubMessage[] | null>(null);
@@ -64,7 +44,7 @@ export default function TextMobileStepper() {
 
   return (
     <Box sx={{ width: '100%', flexGrow: 1 }}>
-      <Typography sx={{ height: "4ex", align: "center" }}>Message</Typography>
+      <Typography sx={{ height: "4ex", textAlign: "center" }}>Message</Typography>
       <Paper
         square
         elevation={10}
