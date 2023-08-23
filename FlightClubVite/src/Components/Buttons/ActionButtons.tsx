c796be8d3ee65a94a13adf05715e479c7e2776ce
YLPilot <yos.1965@gmail.com>
2023-08-23 14:11:00 +0300

    AcctionButton
    Add disable support
    Add state

--- FlightClubVite/src/Components/Buttons/ActionButtons.tsx
+++ FlightClubVite/src/Components/Buttons/ActionButtons.tsx
@@ -6,24 +6,27 @@ import EditIcon from '@mui/icons-material/Edit';
 import PaymentIcon from '@mui/icons-material/Payment';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import CloseIcon from '@mui/icons-material/Close';
-import { keys } from '@mui/system';
 export enum EAction {
   "ADD", "DELETE", "SAVE", "EDIT", "PAY", "ORDER", "CLOSE"
 }
-export interface IActionDispaly {
-  key: EAction, value: string
+export interface IActionDispaly<T> {
+  key: EAction, value: T
 }
+export type ActionColor = "success" | "inherit" | "primary" | "secondary" | "error" | "info";
 export interface IActionButtonsProps {
   show: EAction[],
   OnAction: (action: EAction, event?: React.MouseEvent<HTMLButtonElement, MouseEvent>, item?: string) => void,
   item: string | any,
-  display?: IActionDispaly[]
+  display?: IActionDispaly<string>[],
+  color?: IActionDispaly<ActionColor>[],
+  disable?: IActionDispaly<boolean>[]
 }
 
-function ActionButtons({ OnAction, show, item = "", display }: IActionButtonsProps) {
+
+function ActionButtons({ OnAction, show, item = "", display ,color,disable}: IActionButtonsProps) {
 
   const getName = (findKey: EAction,defaultName: string): string => {
-    CustomLogger.log("ActionButtons/getName/",findKey,display)
+    CustomLogger.info("ActionButtons/getName/key_display",findKey,display)
     if (display !== undefined) {
       let newName = display.find((key) => key.key === findKey)?.value
       CustomLogger.info("ActionButtons/getName/newName",newName)
@@ -32,15 +35,38 @@ function ActionButtons({ OnAction, show, item = "", display }: IActionButtonsPro
     }
     return defaultName
   }
+  const getColor = (findKey: EAction,defaultColor: ActionColor): ActionColor => {
+    CustomLogger.info("ActionButtons/getColor/key_color",findKey,color)
+    if (color !== undefined) {
+      let newName  = color.find((key) => key.key === findKey)?.value
+      CustomLogger.info("ActionButtons/getColor/color", newName,defaultColor)
+      newName =  newName === undefined ? defaultColor : newName
+      CustomLogger.info("ActionButtons/getColor/newName", newName)
+      return newName;
+    }
+    return defaultColor
+  }
+  const getDisable = (findKey: EAction,defaultDisable: boolean): boolean => {
+    CustomLogger.info("ActionButtons/getColor/key_enable",findKey,disable)
+    if (disable !== undefined) {
+      let newName  = disable.find((key) => key.key === findKey)?.value
+      CustomLogger.info("ActionButtons/getDisable/enable", newName,defaultDisable)
+      newName =  newName === undefined ? defaultDisable : newName
+      CustomLogger.info("ActionButtons/getDisable/newName", newName)
+      return newName;
+    }
+    return defaultDisable;
+  }
+
   return (
     <>
-      {show.includes(EAction.ADD) == true && <Button key={"Add"} variant='outlined' onClick={(event) => OnAction(EAction.ADD, undefined, item)} color='success' startIcon={<AddCircleOutlineIcon />}>{getName(EAction.ADD,"Add")}</Button>}
-      {show.includes(EAction.EDIT) == true && <Button key={"Edit"} variant='outlined' onClick={(event) => OnAction(EAction.EDIT, undefined, item)} startIcon={<EditIcon />}>{getName(EAction.EDIT,"Edit")}</Button>}
-      {show.includes(EAction.DELETE) == true && <Button key={"Delete"} variant='outlined' onClick={(event) => OnAction(EAction.DELETE, undefined, item)} color='secondary' startIcon={<DeleteIcon />}>{getName(EAction.DELETE,"Delete")}</Button>}
-      {show.includes(EAction.ORDER) == true && <Button key={"Order"} variant='outlined' onClick={(event) => OnAction(EAction.ORDER, undefined, item)} startIcon={<AddShoppingCartIcon />}>{getName(EAction.ORDER,"Order")}</Button>}
-      {show.includes(EAction.PAY) == true && <Button key={"Pay"} variant='outlined' onClick={(event) => OnAction(EAction.PAY, undefined, item)} startIcon={<PaymentIcon />}>{getName(EAction.PAY,"Pay")}</Button>}
-      {show.includes(EAction.CLOSE) == true && <Button key={"Close"} variant='outlined' onClick={(event) => OnAction(EAction.CLOSE, undefined, item)} startIcon={<CloseIcon />}>{getName(EAction.CLOSE,"Close")}</Button>}
-      {show.includes(EAction.SAVE) == true && <Button key={"Save"} variant='outlined' onClick={(event) => OnAction(EAction.SAVE, undefined, item)} startIcon={<Save />}>{getName(EAction.SAVE,"Save")}</Button>}
+      {show.includes(EAction.ADD) == true && <Button disabled= {getDisable(EAction.ADD ,false)} key={"Add"} variant='outlined' onClick={(event) => OnAction(EAction.ADD, undefined, item)} color={getColor(EAction.ADD,'success')} startIcon={<AddCircleOutlineIcon />}>{getName(EAction.ADD,"Add")}</Button>}
+      {show.includes(EAction.EDIT) == true && <Button disabled= {getDisable(EAction.EDIT,false)} key={"Edit"} variant='outlined' onClick={(event) => OnAction(EAction.EDIT, undefined, item)} color={getColor(EAction.EDIT,'primary')} startIcon={<EditIcon />}>{getName(EAction.EDIT,"Edit")}</Button>}
+      {show.includes(EAction.DELETE) == true && <Button disabled= {getDisable(EAction.DELETE ,false)} key={"Delete"} variant='outlined' onClick={(event) => OnAction(EAction.DELETE, undefined, item)} color={getColor(EAction.DELETE,'secondary')} startIcon={<DeleteIcon />}>{getName(EAction.DELETE,"Delete")}</Button>}
+      {show.includes(EAction.ORDER) == true && <Button disabled= {getDisable(EAction.ORDER,false)} key={"Order"} variant='outlined' onClick={(event) => OnAction(EAction.ORDER, undefined, item)} color={getColor(EAction.ORDER,'primary')} startIcon={<AddShoppingCartIcon />}>{getName(EAction.ORDER,"Order")}</Button>}
+      {show.includes(EAction.PAY) == true && <Button disabled= {getDisable(EAction.PAY,false)}key={"Pay"} variant='outlined' onClick={(event) => OnAction(EAction.PAY, undefined, item)} color={getColor(EAction.PAY,'primary')} startIcon={<PaymentIcon />}>{getName(EAction.PAY,"Pay")}</Button>}
+      {show.includes(EAction.CLOSE) == true && <Button disabled= {getDisable(EAction.CLOSE,false)} key={"Close"} variant='outlined' onClick={(event) => OnAction(EAction.CLOSE, undefined, item)} color={getColor(EAction.CLOSE,'primary')} startIcon={<CloseIcon />}>{getName(EAction.CLOSE,"Close")}</Button>}
+      {show.includes(EAction.SAVE) == true && <Button disabled= {getDisable(EAction.SAVE,false)} key={"Save"} variant='outlined' onClick={(event) => OnAction(EAction.SAVE, undefined, item)} color={getColor(EAction.SAVE,'primary')} startIcon={<Save />}>{getName(EAction.SAVE,"Save")} </Button>}
     </>
   )
 }
