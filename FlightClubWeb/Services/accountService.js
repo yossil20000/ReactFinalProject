04f005faa7b8c86e9850ec07fe8a2b722d18b06f
YLPilot <yos.1965@gmail.com>
2024-01-08 18:55:51 +0200

    Fix Account Member_type not read

--- FlightClubWeb/Services/accountService.js
+++ FlightClubWeb/Services/accountService.js
@@ -2,7 +2,7 @@ const {Account} = require('../Models/account');
 
 const findAccount = async (query) => {
   const accounts = await Account.find(query).populate([
-    {path: "member", model: "Member", select: "_id member_id first_name family_name member_type, id_number"},
+    {path: "member", model: "Member", select: "_id member_id first_name family_name member_type id_number"},
     {path: "transactions",populate:[{path: "order",model:"Order"}]}] );
   return {accounts};
 }
