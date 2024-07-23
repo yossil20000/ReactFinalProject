f557d5987be2ee14c8f2cc9764f7c202950738e3
YLPilot <yos.1965@gmail.com>
2024-07-23 20:56:07 +0300

    2.0.12.4

--- FlightClubVite/src/Pages/Login/VersionHistory.tsx
+++ FlightClubVite/src/Pages/Login/VersionHistory.tsx
@@ -39,7 +39,9 @@ function VersionHistory({show=false} : IVersionHistoryProps) {
     {id: 18,version: "2.0.11.5",date: new Date(2024,7,12).getDisplayDate() , "description" : "Members filter by member state. Reciept add description"  ,"known_bug": "Admin/Devices Device need to clear and reselect the device name"},
     {id: 19,version: "2.0.12.0",date: new Date(2024,7,19).getDisplayDate() , "description" : "Add Order Expense Dialog, fix Member Account view"  ,"known_bug": "Admin/Devices Device need to clear and reselect the device name"},
     {id: 20,version: "2.0.12.1",date: new Date(2024,7,20).getDisplayDate() , "description" : "Fix Transaction dialog, Switch D.Balance / S.Balance position"  ,"known_bug": "Admin/Devices Device need to clear and reselect the device name"},
-    {id: 20,version: "2.0.12.2",date: new Date(2024,7,20).getDisplayDate() , "description" : "Add flight duration column to flight tab"  ,"known_bug": "Admin/Devices Device need to clear and reselect the device name"},
+    {id: 21,version: "2.0.12.2",date: new Date(2024,7,20).getDisplayDate() , "description" : "Add flight duration column to flight tab"  ,"known_bug": "Admin/Devices Device need to clear and reselect the device name"},
+    {id: 22,version: "2.0.12.3",date: new Date(2024,7,20).getDisplayDate() , "description" : "Fix clean display after Place Flight Order"  ,"known_bug": "Admin/Devices Device need to clear and reselect the device name"},
+    {id: 23,version: "2.0.12.4",date: new Date(2024,7,23).getDisplayDate() , "description" : "accont/transaction/add transaction update previous balance"  ,"known_bug": "Admin/Devices Device need to clear and reselect the device name"},
     
   ]
   function CustomToolbar() {
