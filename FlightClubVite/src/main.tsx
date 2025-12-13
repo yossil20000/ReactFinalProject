0aee110bb70e61fdc947b3c8bb5109cdbcc9bddd
YLPilot <yos.1965@gmail.com>
2025-12-13 06:00:48 +0200

    Add Them, StrictMode

--- FlightClubVite/src/main.tsx
+++ FlightClubVite/src/main.tsx
@@ -1,17 +1,23 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { Provider } from 'react-redux'
-import { storeUser } from './app/userStor';
-import { CssBaseline } from '@mui/material'
-import { CCustomLogger } from './customLogging'
-globalThis.CustomLogger = new CCustomLogger({level: 'info'})
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Provider } from "react-redux";
+import { storeUser } from "./app/userStor";
+import { CssBaseline } from "@mui/material";
+import { CCustomLogger } from "./customLogging";
+import { StrictMode } from "react";
+import { ThemeProvider } from "@emotion/react";
+globalThis.CustomLogger = new CCustomLogger({ level: "info" });
+ReactDOM.createRoot(document.getElementById("root")!).render(
   //<Provider store={store}>
-  <Provider store={storeUser}>
-    <CssBaseline/>
-      <App />
-  </Provider>
-  
+  <StrictMode>
+    <ThemeProvider theme={{}}>
+      <Provider store={storeUser}>
+        <CssBaseline />
+        <App />
+      </Provider>
+    </ThemeProvider>
+  </StrictMode>
+
   //</Provider>
-)
+);
